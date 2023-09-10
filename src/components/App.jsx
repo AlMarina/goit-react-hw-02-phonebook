d8c39308ecd27614adb7b9e5e682c8a304979939
AlMarina <almarina1017@gmail.com>
2023-09-10 13:42:37 +0300

    css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = data => {
+    const { contacts } = this.state;
+
+    const isExist = contacts.find(
+      contact => contact.name.toLowerCase() === data.name.toLowerCase()
+    );
+
+    if (isExist) {
+      alert(`${data.name} is already in contacts.`);
+      return;
+    }
+
+    const newContact = {
+      ...data,
+      id: nanoid(),
+    };
+    console.log(newContact);
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  filterSearch = evt => {
+    this.setState({
+      filter: evt.currentTarget.value.toLowerCase(),
+    });
+  };
+
+  filterContact = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter)
+    );
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { filter } = this.state;
+    const contactOfFiltred = this.filterContact();
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.filterSearch} />
+        <ContactList
+          contacts={contactOfFiltred}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
